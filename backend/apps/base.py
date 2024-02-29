e63c6ba4e9456e9f6000dc6ef46e59400da7fb2c
Cmatrix1 <sadeghmajidi980@gmail.com>
2024-02-29 13:38:14 +0330

    add: routers for create update and detail of questions and add addional tags to navbar templates

--- backend/apps/base.py
+++ backend/apps/base.py
@@ -1,8 +1,17 @@
 from apps import route_questions
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app_router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@app_router.get("/", response_class=HTMLResponse)
+def main_page(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
+
 
 app_router.include_router(
-    route_questions.router, prefix="", tags=["questions"], include_in_schema=False
+    route_questions.router, prefix="/question", tags=["questions"], include_in_schema=False
 )
--- backend/apps/route_questions.py
+++ backend/apps/route_questions.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+from db.session import get_db
+from db.repository.questions import list_questions, detail_questions
 
 
 router = APIRouter()
@@ -8,26 +11,38 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/", response_class=HTMLResponse)
-def read_item(request: Request):
-    questions = [
-        {
-            'question': 'What is Python?',
-            'answer': 'Python is a high-level programming language...',
-            'created_at': '2024-02-25',
-            'tags': ['Python', 'Programming', 'Language']
-        },
-        {
-            'question': 'What is AI?',
-            'answer': 'AI stands for Artificial Intelligence...',
-            'created_at': '2024-02-24',
-            'tags': ['AI', 'Technology']
-        },
-        {
-            'question': 'How to sort a list in Python?',
-            'answer': 'You can sort a list in Python using the sort() method...',
-            'created_at': '2024-02-23',
-            'tags': ['Python', 'Programming']
-        }
-    ]
+def question_list(request: Request, db: Session = Depends(get_db)):
+    questions = list_questions(db=db)
+    return templates.TemplateResponse("questions/list.html",
+                                      {"request": request, "questions": questions})
 
-    return templates.TemplateResponse("questions/list.html", {"request": request, "questions": questions})
+
+@router.get("/detail/{id}", response_class=HTMLResponse)
+def question_detail(request: Request, id: int, db: Session = Depends(get_db)):
+    question = detail_questions(id=id, db=db)
+    return templates.TemplateResponse("questions/detail.html", {"request": request, "question": question})
+
+
+@router.get("/create", response_class=HTMLResponse)
+def question_create(request: Request):
+    return templates.TemplateResponse("questions/create.html", {"request": request})
+
+
+@router.post("/create", response_class=HTMLResponse)
+def question_create(request: Request, db: Session = Depends(get_db), question_text: str = Form(...), answer: str = Form(...), level: str = Form(...), tags: list[str] = Form(...), references: list[str] = Form(...)):
+    print(f"{question_text=}, {answer=}, {level=}, {tags=}, {references=}")
+    # Implement the logic of create question
+    return templates.TemplateResponse("questions/create.html", {"request": request})
+
+
+@router.get("/update/{id}", response_class=HTMLResponse)
+def question_update(request: Request, id: int, db: Session = Depends(get_db)):
+    question = detail_questions(id=id, db=db)
+    return templates.TemplateResponse("questions/update.html", {"request": request, "question": question})
+
+@router.get("/update/{id}", response_class=HTMLResponse)
+def question_update(request: Request, db: Session = Depends(get_db), question_text: str = Form(...), answer: str = Form(...), level: str = Form(...), tags: list[str] = Form(...), references: list[str] = Form(...)):
+    question = detail_questions(id=id, db=db)
+    print(f"{question_text=}, {answer=}, {level=}, {tags=}, {references=}")
+    # Implement the logic of update question
+    return templates.TemplateResponse("questions/update.html", {"request": request, "question": question})
