42febc4ffc6289e9df8adb09a78bc37e7b39f948
Cmatrix1 <sadeghmajidi980@gmail.com>
2024-02-29 13:34:40 +0330

    add: shcema for detail question

---
+++ backend/schemas/question.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import List
+
+class Tag(BaseModel):
+    ...
+
+class Reference(BaseModel):
+    ...
+
+class DetailQuestion(BaseModel):
+    id: int
+    question_text: str
+    answer: str
+    level: str
+    tags: List[Tag] = []
+    references: List[Reference] = []
+    
+    next_question_id: int | None = None
+    prev_question_id: int | None = None
+
