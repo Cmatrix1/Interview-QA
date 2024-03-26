8efa56d85ace6d321081405d4467e7fb50171075
Cmatrix1 <sadeghmajidi980@gmail.com>
2024-03-26 23:48:28 +0330

    add: make the users querys better for each use case

--- backend/db/repository/questions.py
+++ backend/db/repository/questions.py
@@ -1,8 +1,15 @@
 from db.models.question import Question
-# from schemas.question import
 from sqlalchemy import func
 from sqlalchemy.orm import Session
+from db.models.tag import Tag
+from db.models.refrence import Reference
 from schemas.question import DetailQuestion
+from utils.exceptions.questions import QuestionNotFoundException
+
+
+def question_exist(question_id: int, db: Session) -> bool:
+    question = db.query(Question.id).filter(Question.id == question_id).scalar()
+    return bool(question)
 
 
 def list_questions(db: Session) -> list[Question]:
@@ -32,4 +39,36 @@ def detail_questions(id: int, db: Session) -> DetailQuestion:
             next_question_id=next_question,
             prev_question_id=prev_question
             )
-    return None
+    raise QuestionNotFoundException(f"Question with ID {id} not found")
+
+
+def create_question(question_text: str, answer: str, level: str, tags: list[Tag], references: list[Reference], db: Session):
+    question_object = Question(
+        question_text=question_text,
+        answer=answer,
+        level=level
+    )
+    question_object.tags.extend(tags)
+    question_object.references.extend(references)
+    db.add(question_object)
+    db.commit()
+    return question_object
+
+
+def update_question(question_id: int, question_text: str, answer: str, level: str, tags: list[Tag], references: list[Reference], db: Session):
+    question_object = db.query(Question).filter(Question.id == question_id).first()
+    if not question_object:
+        raise QuestionNotFoundException(f"Question with ID {question_id} not found")
+
+    question_object.question_text = question_text
+    question_object.answer = answer
+    question_object.level = level
+
+    question_object.tags.clear()
+    question_object.references.clear()
+
+    question_object.tags.extend(tags)
+    question_object.references.extend(references)
+
+    db.commit()
+    return question_object
--- backend/db/repository/user.py
+++ backend/db/repository/user.py
@@ -20,8 +20,15 @@ def create_new_user(user: UserCreate, db: Session):
     return user
 
 
+def get_user_for_authentication(username: str, db: Session, raise_exceptions: bool = True) -> User | None:
+    user = db.query(User.id, User.username).filter(User.username == username).first()
+    if user is None and raise_exceptions:
+        raise UserNotFoundException()
+    return user
+
+
 def get_user(username: str, db: Session, raise_exceptions: bool = True) -> User | None:
-    user = db.query(User).filter(User.username == username).scalar()
+    user = db.query(User.id, User.username, User.password).filter(User.username == username).first()
     if user is None and raise_exceptions:
         raise UserNotFoundException()
     return user
